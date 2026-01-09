248d90702b02d0ca5b46eaf01c46b95f29d4b484
deepak5671 <deepak183654@gmail.com>
2026-01-09 22:16:18 +0530

    Add files via upload

---
+++ arrays/JumpGame.java
@@ -0,0 +1,33 @@
+/*
+Problem Statement (Jump Game I)
+You are given an integer array nums where each element represents the maximum jump length you can make from that position.
+- Initially, you are at the first index.
+- Your goal is to reach the last index.
+- Return true if you can reach the last index, otherwise return false.
+
+Input: nums = [2,3,1,1,4]
+Output: true
+Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+*/
+
+public class JumpGame {
+    public boolean canJump(int[] nums) {
+        int reachable = 0; // farthest index we can reach
+        for (int i = 0; i < nums.length; i++) {
+            if (i > reachable) {
+                return false; // current index not reachable
+            }
+            reachable = Math.max(reachable, i + nums[i]);
+        }
+        return true;
+    }
+
+    // Example usage
+    public static void main(String[] args) {
+        JumpGame game = new JumpGame();
+        int[] nums1 = {2,3,1,1,4};
+        int[] nums2 = {3,2,1,0,4};
+        System.out.println(game.canJump(nums1)); // true
+        System.out.println(game.canJump(nums2)); // false
+    }
+}
